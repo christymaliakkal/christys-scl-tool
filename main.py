c60a862bd58cee0ae96d2683f3a40b2fb73be701
christymaliakkal <christymaliakkal25@gmail.com>
2026-07-31 12:47:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Christy's SCL Tool</title>
+<link rel="stylesheet" href="/static/bwxt_theme.css" />
+</head>
+<body>
+<h1>Welcome to Christy's SCL Tool. Upload your PDF to begin.</h1>
+<form action="/process" method="post" enctype="multipart/form-data">
+<input type="file" name="file" />
+<button type="submit">Process</button>
+</form>
+</body>
+</html>
